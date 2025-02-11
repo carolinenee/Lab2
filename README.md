c4611983c5d23d7412b4d04f51435924dfb3f2ce
carolinenee <caroline.nee@mail.utoronto.ca>
2025-02-10 23:50:07 -0500

    added snow and details to the website

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Lab2
- ggr472 second lab 
+ Web map made for GGR472 lab 2 
+ It presents data on petstores downtown made on geojson.io 
+ It also shows the locations of bike docking stations to help visualize how 
+ convenient the pet stores are for people using a bike to get there 
+ 
+ The pink dots represent the locations of pet stores and are replaced by the 
+ name of the petstore when you zoom in closer to it 
+ 
+ The basemap was adapted form a mapbox template 
--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiY2Fyb2xpbmVuZWUiLCJhIjoiY201b2RhZmxtMGthajJucHRxcW5heGxiNyJ9.NMKAQoQvhYJ8RQq0NQuYkA'; // Add default public map token from your Mapbox account
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2Fyb2xpbmVuZWUiLCJhIjoiY201b2RhZmxtMGthajJucHRxcW5heGxiNyJ9.NMKAQoQvhYJ8RQq0NQuYkA'; // default public map token from my Mapbox account
 const map = new mapboxgl.Map({
     container: 'my-map', // map container ID
-    style: 'mapbox://styles/carolinenee/cm6zbbxvf000n01qwgyu3elks', // style URL
-    center: [-79.392221, 43.655417], // starting position [lng, lat]
+    style: 'mapbox://styles/carolinenee/cm6zbbxvf000n01qwgyu3elks', //  URL link for mapbox style that I made
+    center: [-79.392221, 43.655417], // starting position [lng, lat] (moozda location)
     zoom: 18, // starting zoom level
 });
-// adding polygon location to map 
 map.on('load', () => {
-    // feature collection was copy pasted directly from geojson.io 
+    //add bike parking locations 
+    // Add a data source from a GeoJSON file
+    map.addSource('bike-data', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/carolinenee/Lab2/refs/heads/main/wk5-data/Bicycle%20Parking%20Map%20Data%20-%204326.geojson' // Your URL to bike geojson file that I published on github
+    });
+    map.addLayer({
+        'id': 'bike-docks',
+        'type': 'circle',
+        'source': 'bike-data', //refers to the map.on source name from above
+        paint: {
+            'circle-radius': 3,  //size of circle 
+            'circle-color': '#000000', //000000 is black 
+            'circle-opacity': 0.5 //makes circles opaque because i thought it looked better 
+        }
+    });
+    // adding polygon location to map 
     map.addSource('MoozdaHome', {
         type: 'geojson',
-        data: {
+        data: {  // feature collection was copy pasted directly from geojson.io 
             "type": "FeatureCollection",
             "features": [
                 {
@@ -53,8 +68,31 @@ map.on('load', () => {
         'type': 'fill',
         'source': 'MoozdaHome', //needs to be same name as in map.addSource
         'paint': {
-            "fill-color": "#FEA3AA", // Red color fill
-            "fill-opacity": 0.6 // 60% transparenc
+            "fill-color": "#FEA3AA", // pink color 
+            "fill-opacity": 0.6 // 60% transparency
         }
     });
+    // Add Snow Effect taken from mapbox example codes
+    const zoomBasedReveal = (value) => {
+        return [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            5, 0.0,
+            20, value
+        ];
+    };
+
+    map.setSnow({
+        density: zoomBasedReveal(0.85),
+        intensity: .70,
+        'center-thinning': 0.1,
+        direction: [0, 50],
+        opacity: 0.80,
+        color: `#ffffff`,
+        'flake-size': 0.21,
+        vignette: zoomBasedReveal(0.3),
+        'vignette-color': `#ffffff`
+    });
+
 });
